d22ab3f7a8cdd212cab81432cb95dc26ae693b6d
Pravin Patel <patelpra337@gmail.com>
2020-07-09 19:28:28 -0500

    Add and update ImagePostViewController and UIImage+Scaling

--- ImageFilterEditor/ImageFilterEditor/ImagePostViewController.swift
+++ ImageFilterEditor/ImageFilterEditor/ImagePostViewController.swift
@@ -13,6 +13,14 @@ import Photos
 
 class ImagePostViewController: UIViewController {
     
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var invertColors: UISwitch!
+    @IBOutlet var vignetteSlider: UISlider!
+    @IBOutlet var sketchifySlider: UISlider!
+    @IBOutlet var kaleidoscopeSlider: UISlider!
+    @IBOutlet var perspectiveSwitch: UISlider!
+    
+    
     private let context = CIContext()
     
     private let invertColorsFilter = CIFilter.colorInvert()
@@ -30,10 +38,64 @@ class ImagePostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-
+        orignalImage = imageView.image
+    }
+    
+    @IBAction func photosButton(_ sender: UIBarButtonItem) {
         
     }
     
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("The photo library is not avaialbe")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func updateImage() {
+        if let orignalImage = orignalImage {
+            imageView.image = orignalImage
+        } else {
+            imageView.image = nil
+        }
+    }
+    
+    private func image(byFiltering image: UIImage) -> UIImage {
+        let inputImage = CIImage(image: image)
+    
+        var outputImage = inputImage
+        
+        if invertColors.isOn {
+            invertColorsFilter.inputImage = outputImage
+            guard let filteredImage = invertColorsFilter.outputImage else {
+                return orignalImage! }
+            outputImage = filteredImage
+            }
+        
+        vignetteFilter.inputImage = outputImage
+        
+    }
+}
 
+extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.editedImage] as? UIImage {
+            orignalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            orignalImage = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
